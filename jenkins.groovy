963fb8d839cfaf93a1f6bcd6acf298928ca5c444
SURIYAPRAKASAHREDDY <155240817+SURIYAPRAKASAHREDDY@users.noreply.github.com>
2025-04-13 19:45:43 +0530

    Update jenkins.groovy
    Groovy updated application availability

--- jenkins.groovy
+++ jenkins.groovy
@@ -13,8 +13,9 @@ pipeline {
             steps {
                 echo "🔧 Running build steps..."
                 sh """
-                  cd /var/lib/jenkins/workspace/new-jenkins/Docker/miniproject
-                  docker-compose -f docker-compose build
+                  cd /var/lib/jenkins/workspace/Jenkins_docker_project/Docker/miniproject/
+                  docker-compose -f docker-compose.yaml down
+                  docker-compose -f docker-compose.yaml build
                   docker-compose -f docker-compose.yaml up -d
                  """
                 sh 'echo "Build successful!"'
@@ -31,9 +32,37 @@ pipeline {
         stage('Deploy') {
             steps {
                 echo "🚀 Deploying application..."
+                sh " docker ps -a "
                 sh 'echo "Deployed successfully!"'
             }
         }
+        stage('Check Application Availability') {
+    steps {
+        script {
+            def publicIp = '3.86.2.216'  // Replace with actual IP or inject as env var
+
+            def ports = [3000, 5000]
+
+            ports.each { port ->
+                def url = "http://${publicIp}:${port}"
+
+                echo "Checking availability for ${url}..."
+
+                def response = sh(
+                    script: "curl -s -o /dev/null -w \"%{http_code}\" ${url}",
+                    returnStdout: true
+                ).trim()
+
+                if (response == '200') {
+                    echo "✅ Application on port ${port} is UP."
+                } else {
+                    error("❌ Application on port ${port} is NOT reachable. Status: ${response}")
+                }
+            }
+        }
+    }
+}
+
     }
 
     post {
